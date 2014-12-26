d9685e8ac8791f29d4eccd73c98afa58695b4bb8
Manuel Vila <mvila@3base.com>
2014-12-26 19:15:03 +0800

    add diamond problem test

--- test.js
+++ test.js
@@ -4,65 +4,92 @@ var assert = require('chai').assert;
 var KindaClass = require('./');
 
 suite('KindaClass', function() {
-  var Foo = KindaClass.extend('Foo', function() {
-    this.cool = 'very';
+  suite('Simple classes hierarchy', function() {
+    var Foo = KindaClass.extend('Foo', function() {
+      this.cool = 'very';
+    });
+
+    Foo.hello = 'Hello';
+    Foo._bye = 'Bye';
+
+    var Bar = Foo.extend('Bar', function() {
+      this.isCold = function() {
+        return this.cool === 'very' ? 'yes' : 'no';
+      };
+    });
+
+    var Baz = KindaClass.extend('Baz', function() {
+      this.include(Bar);
+    });
+
+    suite('Class methods', function() {
+      test('extend', function() {
+        assert.equal(Bar.hello, 'Hello');
+        assert.isUndefined(Bar._bye);
+      });
+
+      test('instantiate', function() {
+        var bar = Bar.instantiate();
+        assert.equal(bar.cool, 'very');
+        assert.isFunction(bar.isCold);
+      });
+
+      test('getPrototype', function() {
+        var bar = Bar.instantiate();
+        assert.equal(Bar.getPrototype().isCold, bar.isCold);
+      });
+
+      test('isKindaClass', function() {
+        assert.ok(Bar.isKindaClass());
+      });
+    });
+
+    suite('Instance methods', function() {
+      test('include', function() {
+        var baz = Baz.instantiate();
+        assert.equal(baz.cool, 'very');
+        assert.isFunction(baz.isCold);
+      });
+
+      test('getClass', function() {
+        var foo = Foo.instantiate();
+        assert.equal(foo.getClass(), Foo);
+      });
+
+      test('getPrototype', function() {
+        var bar = Bar.instantiate();
+        assert.equal(Bar.getPrototype().isCold, bar.isCold);
+      });
+
+      test('isInstanceOf', function() {
+        var foo = Foo.instantiate();
+        assert.ok(foo.isInstanceOf(Foo));
+      });
+    });
   });
 
-  Foo.hello = 'Hello';
-  Foo._bye = 'Bye';
+  suite('Diamond problem', function() {
+    var count = 0;
 
-  var Bar = Foo.extend('Bar', function() {
-    this.isCold = function() {
-      return this.cool === 'very' ? 'yes' : 'no';
-    };
-  });
-
-  var Baz = KindaClass.extend('Baz', function() {
-    this.include(Bar);
-  });
-
-  suite('class methods', function() {
-    test('extend', function() {
-      assert.equal(Bar.hello, 'Hello');
-      assert.isUndefined(Bar._bye);
+    var Top = KindaClass.extend('Top', function() {
+      count++;
     });
 
-    test('instantiate', function() {
-      var bar = Bar.instantiate();
-      assert.equal(bar.cool, 'very');
-      assert.isFunction(bar.isCold);
+    var Left = Top.extend('Left');
+
+    var Right = Top.extend('Right');
+
+    var Bottom = Top.extend('Bottom', function() {
+      this.include(Left);
+      this.include(Right);
     });
 
-    test('getPrototype', function() {
-      var bar = Bar.instantiate();
-      assert.equal(Bar.getPrototype().isCold, bar.isCold);
-    });
-
-    test('isKindaClass', function() {
-      assert.ok(Bar.isKindaClass());
-    });
-  });
-
-  suite('instance methods', function() {
-    test('include', function() {
-      var baz = Baz.instantiate();
-      assert.equal(baz.cool, 'very');
-      assert.isFunction(baz.isCold);
-    });
-
-    test('getClass', function() {
-      var foo = Foo.instantiate();
-      assert.equal(foo.getClass(), Foo);
-    });
-
-    test('getPrototype', function() {
-      var bar = Bar.instantiate();
-      assert.equal(Bar.getPrototype().isCold, bar.isCold);
-    });
-
-    test('isInstanceOf', function() {
-      var foo = Foo.instantiate();
-      assert.ok(foo.isInstanceOf(Foo));
+    test('Top constructor should only be called once', function() {
+      assert.equal(count, 0);
+      var bottom1 = Bottom.instantiate();
+      assert.equal(count, 1);
+      var bottom2 = Bottom.instantiate();
+      assert.equal(count, 1);
     });
   });
 });
